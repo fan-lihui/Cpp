d18e144a68500b8758e7244c91fa4da24c36dd90
fan-lihui <118170898+fan-lihui@users.noreply.github.com>
2023-05-31 22:20:06 +0800

    Add files via upload

---
+++ 指针所占内存空间.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+//int main()
+//{
+//	//指针所占内存空间
+//	int a = 10;
+//	//int * p;
+//	//p = &a;
+//	int* p = &a;
+//
+//	//在32位操作系统（x86）下，指针是占4个字节空间大小，不管什么数据类型
+//	//在64位操作系统下，指针是占8个字节空间大小
+//	cout << "int* = " << sizeof(int*) << endl; //int* = 4
+//	cout << "float* = " << sizeof(float*) << endl; //float* = 4
+//	cout << "double* = " << sizeof(double*) << endl; //double* = 4
+//	cout << "char* = " << sizeof(char*) << endl; //char* = 4
+//	cout << "long* = " << sizeof(long*) << endl; //long* = 4
+//
+//
+//	return 0;
+//}
